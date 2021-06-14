9a9e8af4313d3487a877267f1a10c465b5c83645
ben <benegan435@gmail.com>
2021-06-14 16:18:17 +0100

    we don't know what we did

--- SendText.jsx
+++ SendText.jsx
@@ -1,57 +1,56 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, TextInput, Button, StyleSheet} from 'react-native'
 import axios from 'axios';
+import API_KEY from './API_KEY'
 
 const SendText = () => {
-    const [contactNumber, setContactNumber] = useState(123)
-    const [message, setMessage] = useState("hello")
+    const [contactNumber, setContactNumber] = useState("")
+    const [message, setMessage] = useState("")
     //const [payload, setPayload] = useState([null, null, null])
+    const url = "https://api.thesmsworks.co.uk/v1/message/send"
 
+    const apiURL = axios.create({   
+        "baseURL": 'https://api.thesmsworks.co.uk/v1/message/send',
+        "headers": {
+        "Authorization": API_KEY
+        }
+    }); 
      
     const sendMessage = (contactNumber,message) => {
-        // contsruct http request
-        // send it
-        // console.log the response
-     
-
-          const token = 'insertkey'
-          const url = 'https://api.thesmsworks.co.uk/v1/message/send'
-
-          axios.post(url, {
+          axios.post(url, { 
             "sender": "ramblr-dev",
             "destination": contactNumber,
-            "content" : message
-          }, {
-            "headers": {
-            'Content-Type' : 'application/json',
-            'Authorization': token,
-            'Access-Control-Allow-Headers': 'Authorization',
-            'Access-Control-Allow-Origin' : '*',   
-            }
-          }).then(res => console.log(res))
-          .catch(err => console.log("it broke here: ",err))
-
-
+            "content" : message   
+        }).then(res => console.log(res))
+          .catch((err) => {console.log("it broke here: ",err)})
     }
 
+
+
+
     return (
-        <View>
-            <View>
+
+        <>
                 contact number: <TextInput style={styles.input} value={contactNumber} placeholder="enter number" onChangeText={setContactNumber} keyboardType="numeric"/>
                 message:<TextInput style={styles.input} value={message} placeholder="enter message" onChangeText={setMessage}/>
 
                 <Button title="send" 
                 defaultValue={contactNumber}
                 onPress={()=>sendMessage(contactNumber, message)}
-                />  
-                <View>
-                    <Text>Contact number in state: {contactNumber}</Text>
-                </View>
-                <View>
-                    <Text>Message in state: {message}</Text>
-                </View>  
+                />
+        <View >
+            <Text>Contact number in state: {contactNumber}<br/></Text>
+            <Text>Message in state: {message}</Text>
+
             </View>
-        </View>
+
+           
+        </>
+
+        
+
+
+
     );
 
 
