7cac3647f29d973f70e64c71ba905fd8be8f23c8
Christian Dutton <Dutton1990@googlemail.com>
2021-06-15 09:20:28 +0100

    text sending successfully

--- SendText.jsx
+++ SendText.jsx
@@ -1,67 +1,79 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, TextInput, Button, StyleSheet} from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import axios from 'axios';
-import API_KEY from './API_KEY'
+import API_KEY from './API_KEY.env';
 
 const SendText = () => {
-    const [contactNumber, setContactNumber] = useState("")
-    const [message, setMessage] = useState("")
-    //const [payload, setPayload] = useState([null, null, null])
-    const url = "https://api.thesmsworks.co.uk/v1/message/send"
+  const [contactNumber, setContactNumber] = useState('');
+  const [message, setMessage] = useState('');
 
-    const apiURL = axios.create({   
-        "baseURL": 'https://api.thesmsworks.co.uk/v1/message/send',
-        "headers": {
-        "Authorization": API_KEY
-        }
-    }); 
-     
-    const sendMessage = (contactNumber,message) => {
-          axios.post(url, { 
-            "sender": "ramblr-dev",
-            "destination": contactNumber,
-            "content" : message   
-        }).then(res => console.log(res))
-          .catch((err) => {console.log("it broke here: ",err)})
-    }
+  const url = 'https://api.thesmsworks.co.uk/v1/message/send';
 
+  // axios.interceptors.request.use(
+  //   (config) => {
+  //     config.headers.authorization = API_KEY;
 
+  //     return config;
+  //   },
+  //   (err) => {
+  //     return Promise.reject(err);
+  //   }
+  // );
 
-
-    return (
-
-        <>
-                contact number: <TextInput style={styles.input} value={contactNumber} placeholder="enter number" onChangeText={setContactNumber} keyboardType="numeric"/>
-                message:<TextInput style={styles.input} value={message} placeholder="enter message" onChangeText={setMessage}/>
-
-                <Button title="send" 
-                defaultValue={contactNumber}
-                onPress={()=>sendMessage(contactNumber, message)}
-                />
-        <View >
-            <Text>Contact number in state: {contactNumber}<br/></Text>
-            <Text>Message in state: {message}</Text>
-
-            </View>
-
-           
-        </>
-
-        
-
-
-
+  const sendMessage = (contactNumber, message) => {
+    axios({
+      method: 'post',
+      url: url,
+      data: {
+        sender: 'ramblr',
+        destination: contactNumber,
+        content: message,
+      },
+    }).then(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
     );
+  };
 
+  return (
+    <>
+      <TextInput
+        style={styles.input}
+        value={contactNumber}
+        placeholder="enter number"
+        onChangeText={setContactNumber}
+        keyboardType="numeric"
+      />
 
+      <TextInput
+        style={styles.input}
+        value={message}
+        placeholder="enter message"
+        onChangeText={setMessage}
+      />
+      <Button
+        title="send"
+        defaultValue={contactNumber}
+        onPress={() => sendMessage(contactNumber, message)}
+      />
+      <View>
+        <Text>Contact number in state: {contactNumber}</Text>
+        <Text>Message in state: {message}</Text>
+      </View>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-    input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-    },
-  });
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+});
 
 export default SendText;
